9a4e59eb654294827b8fbfdde090a56e4fcd288a
libo <370157506@qq.com>
2021-04-11 10:25:43 +0800

    feat：web-sys canvas 例子

--- core/index.js
+++ core/index.js
@@ -1,66 +1,11 @@
-const { ipcRenderer } = require('electron')
+// Note that a dynamic `import` statement here is required due to
+// webpack/webpack#6615, but in theory `import { greet } from './pkg';`
+// will work here one day as well!
+const rust = import('@pkg');
 
-let input = document.querySelector('input');
-let orgin = document.querySelector('#orgin');
-let photo = document.querySelector('#photo');
 
-let orgin_ctx = orgin.getContext("2d");
-let photo_ctx = photo.getContext("2d");
-
-console.log(photo_ctx, orgin_ctx)
-input.addEventListener('change', (e) => {
-  console.log(1111)
-  handleImage(e, orgin, orgin_ctx).then((img) => {
-    console.log(22222)
-    let imageDate = img.ctx.getImageData(0, 0, img.width, img.height);
-    let data = imageDate.data;
-    console.log(console.time('log'))
-    for (let i = 0, len = data.length; i < len; i += 4) {
-      let red = data[i];
-      let green = data[i + 1];
-      let blue = data[i + 2];
-      let alpha = data[i + 3];
-
-      let average = Math.floor((red + green + blue) / 3);
-
-      data[i] = average;
-      data[i + 1] = average;
-      data[i + 2] = average;
-    }
-    console.timeEnd("log")
-    imageDate.data = data;
-    photo.width = img.width;
-    photo.height = img.height;
-    photo_ctx.putImageData(imageDate, 0, 0);
-    return imageDate
-  }).then((imageDate) => {
-    console.log(111111)
-    ipcRenderer.emit('send', imageDate)
+rust
+  .then(m => {
+    // console.log(m)
   })
-})
-
-
-function handleImage(e, canvas, ctx) {
-  return new Promise((reslove) => {
-    let reader = new FileReader();
-    reader.onload = (event) => {
-      console.log(3333)
-      var img = new Image();
-      img.onload = () => {
-        console.log(4444)
-        canvas.width = img.width
-        canvas.height = img.height
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-        reslove({
-          img,
-          ctx,
-          width: canvas.width,
-          height: canvas.height
-        });
-      }
-      img.src = event.target.result;
-    }
-    console.log(5555)
-    reader.readAsDataURL(e.target.files[0]);
-  })
-}
+  .catch(console.error);
--- src/lib.rs
+++ src/lib.rs
@@ -1,73 +1,64 @@
-use nodejs_sys::{
-    napi_callback_info, napi_create_array, napi_create_function, napi_create_int64,
-    napi_create_string_utf8, napi_env, napi_get_array_length, napi_get_arraybuffer_info,
-    napi_get_cb_info, napi_get_element, napi_get_named_property, napi_get_prototype,
-    napi_get_undefined, napi_get_value_string_utf8, napi_has_element, napi_has_named_property,
-    napi_is_array, napi_is_arraybuffer, napi_set_element, napi_set_named_property, napi_value,
-};
+use wasm_bindgen::prelude::*;
+use std::f64;
+use wasm_bindgen::JsCast;
+use web_sys::console;
 
-use std::ffi::CString;
-
-pub unsafe extern "C" fn gray(env: napi_env, info: napi_callback_info) -> napi_value {
-    // creating a buffer of arguments
-    let mut argc = 2 as usize;
-    let mut buffer: [napi_value; 2] = std::mem::MaybeUninit::zeroed().assume_init();
-    // getting arguments
-    napi_get_cb_info(
-        env,
-        info,
-        &mut argc,
-        buffer.as_mut_ptr(),
-        std::ptr::null_mut(),
-        std::ptr::null_mut(),
-    );
-    println!("rust buffer:{:?}",buffer);
-
-    let mut res: napi_value = std::mem::zeroed();
-    let p = CString::new("obj").expect("CString::new failed");
-    let _s = napi_get_named_property(env, buffer[0], p.as_ptr(), &mut res);
-
-    // let mut len = 0;
-    // napi_get_value_string_utf8(env, res, std::ptr::null_mut(), 0, &mut len);
-    // let size = len as usize;
-    // println!("size:{:?}",size);
-
-    let mut ve: Vec<u8> = Vec::with_capacity(1 + 1);
-    let raw = ve.as_mut_ptr();
-    std::mem::forget(ve);
-    let mut cap = 0;
-    let _s = napi_get_value_string_utf8(env, res, raw as *mut i8, 1+1, &mut cap);
-    let s = String::from_raw_parts(raw, cap as usize, 1 + 1);
-    println!("{:?}", s);
-
-    let mut ve: napi_value = std::mem::zeroed();
-    let mut local: napi_value = std::mem::zeroed();
-
-    let p = CString::new("Hello rust").expect("CString::new failed");
-    let _s = napi_create_string_utf8(env, p.as_ptr(), 10, &mut local);
-    // println!("{:?}---{:?}",s,local);
-    let _s = napi_create_array(env, &mut ve);
-    // println!("{:?}",s);
-    let _s = napi_set_element(env, ve, 0, local);
-    // println!("rust:{:?}",s);
-    ve
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
 
-#[no_mangle]
-pub unsafe extern "C" fn napi_register_module_v1(
-    env: napi_env,
-    exports: napi_value,
-) -> nodejs_sys::napi_value {
-    let p = CString::new("gray").expect("CString::new failed");
-    let mut local: napi_value = std::mem::zeroed();
-    napi_create_function(
-        env,
-        p.as_ptr(),
-        4,
-        Some(gray),
-        std::ptr::null_mut(),
-        &mut local,
-    );
-    napi_set_named_property(env, exports, p.as_ptr(), local);
-    exports
+// Next let's define a macro that's like `println!`, only it works for
+// `console.log`. Note that `println!` doesn't actually work on the wasm target
+// because the standard library currently just eats all output. To get
+// `println!`-like behavior in your app you'll likely want a macro like this.
+
+macro_rules! console_log {
+    // Note that this is using the `log` function imported above during
+    // `bare_bones`
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document.get_element_by_id("canvas").unwrap();
+    console::log_1(&canvas);
+    let canvas: web_sys::HtmlCanvasElement = canvas
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|_| ())
+        .unwrap();
+
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    context.begin_path();
+
+    // Draw the outer circle.
+    context
+        .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
+        .unwrap();
+
+    // Draw the mouth.
+    context.move_to(110.0, 75.0);
+    context.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI).unwrap();
+
+    // Draw the left eye.
+    context.move_to(65.0, 65.0);
+    context
+        .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
+        .unwrap();
+
+    // Draw the right eye.
+    context.move_to(95.0, 65.0);
+    context
+        .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
+        .unwrap();
+
+    context.stroke();
 }
