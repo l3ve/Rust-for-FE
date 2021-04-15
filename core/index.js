f402ff7dc60a67aa2b0713a25249bcb439c5b9f4
libo <370157506@qq.com>
2021-04-15 14:53:37 +0800

    feat：pass args

--- core/index.js
+++ core/index.js
@@ -2,10 +2,19 @@
 // webpack/webpack#6615, but in theory `import { greet } from './pkg';`
 // will work here one day as well!
 const rust = import('@pkg');
-
+let obj = {
+  name: 'js'
+}
 
 rust
   .then(m => {
-    // console.log(m)
+    m.greet({ name: 'js' })
+  }).then(()=>{
+    console.log('js:',obj)
   })
   .catch(console.error);
+
+
+export function ll() {
+  return obj
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,64 +1,42 @@
 use wasm_bindgen::prelude::*;
-use std::f64;
-use wasm_bindgen::JsCast;
-use web_sys::console;
+// extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
 
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
+    pub fn alert(s: &str);
+
 }
 
-// Next let's define a macro that's like `println!`, only it works for
-// `console.log`. Note that `println!` doesn't actually work on the wasm target
-// because the standard library currently just eats all output. To get
-// `println!`-like behavior in your app you'll likely want a macro like this.
 
-macro_rules! console_log {
-    // Note that this is using the `log` function imported above during
-    // `bare_bones`
-    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Element {
+    name: String,
 }
+
+#[wasm_bindgen]
+pub fn greet(name: &JsValue) -> f64 {
+
+    let mut ss: Element = name.into_serde().unwrap();
+    ss.name = String::from("rust");
+    log(&format!("2, {:?}!", ss));
+    return 1.0;
+}
+
+
+#[wasm_bindgen(module = "/core/index.js")]
+extern "C" {
+    pub fn ll() -> JsValue;
+
+}
+
 
 #[wasm_bindgen(start)]
 pub fn start() {
-    let document = web_sys::window().unwrap().document().unwrap();
-    let canvas = document.get_element_by_id("canvas").unwrap();
-    console::log_1(&canvas);
-    let canvas: web_sys::HtmlCanvasElement = canvas
-        .dyn_into::<web_sys::HtmlCanvasElement>()
-        .map_err(|_| ())
-        .unwrap();
-
-    let context = canvas
-        .get_context("2d")
-        .unwrap()
-        .unwrap()
-        .dyn_into::<web_sys::CanvasRenderingContext2d>()
-        .unwrap();
-
-    context.begin_path();
-
-    // Draw the outer circle.
-    context
-        .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    // Draw the mouth.
-    context.move_to(110.0, 75.0);
-    context.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI).unwrap();
-
-    // Draw the left eye.
-    context.move_to(65.0, 65.0);
-    context
-        .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    // Draw the right eye.
-    context.move_to(95.0, 65.0);
-    context
-        .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    context.stroke();
+    let a = ll();
+    let mut ss: Element = a.into_serde().unwrap();
+    log(&format!("1, {:?}!", ss));
 }
